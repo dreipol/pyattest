8e6a63c9085bae1bfd3a6f9eaa77ee81f3c9388c
Laila Becker <laila.becker@dreipol.ch>
2024-08-08 16:44:14 +0200

    [play-integrity-improvements] Add test cases for illegal config

--- tests/test_google_play_integrity_api_attestation.py
+++ tests/test_google_play_integrity_api_attestation.py
@@ -7,7 +7,7 @@ from cryptography.hazmat.primitives.serialization import Encoding, PublicFormat
 from pyattest.attestation import Attestation
 from pyattest.configs.google_play_integrity_api import GooglePlayIntegrityApiConfig
 from pyattest.exceptions import PyAttestException, InvalidNonceException, InvalidAppIdException, InvalidAppIntegrity, \
-    InvalidDeviceIntegrity, InvalidKeyIdException
+    InvalidDeviceIntegrity, InvalidKeyIdException, IllegalConfigurationException
 from pyattest.testutils.factories.attestation.google import get_play_integrity_api_attestation
 
 nonce = os.urandom(32)
@@ -172,6 +172,7 @@ def test_strong_device_integrity():
     attestation = Attestation(attestation, nonce, config)
     attestation.verify()
 
+
 def test_missing_strong_device_integrity():
     apk_package_name = 'foo'
     attestation, verification_key, aes_key = get_play_integrity_api_attestation(apk_package_name, nonce,
@@ -189,3 +190,39 @@ def test_missing_strong_device_integrity():
     attestation = Attestation(attestation, nonce, config)
     with raises(InvalidDeviceIntegrity):
         attestation.verify()
+
+
+def test_illegal_config1():
+    apk_package_name = 'foo'
+    _, verification_key, aes_key = get_play_integrity_api_attestation(apk_package_name, nonce,
+                                                                      'UNRECOGNIZED_VERSION',
+                                                                      ['MEETS_DEVICE_INTEGRITY'])
+    with raises(IllegalConfigurationException):
+        GooglePlayIntegrityApiConfig(decryption_key=base64.standard_b64encode(aes_key).decode(),
+                                     verification_key=base64.standard_b64encode(
+                                         verification_key.public_bytes(encoding=Encoding.DER,
+                                                                       format=PublicFormat.SubjectPublicKeyInfo))
+                                     .decode(),
+                                     apk_package_name=apk_package_name,
+                                     production=True,
+                                     allow_non_play_distribution=True,
+                                     verify_code_signature_hex=None
+                                     )
+
+
+def test_illegal_config2():
+    apk_package_name = 'foo'
+    _, verification_key, aes_key = get_play_integrity_api_attestation(apk_package_name, nonce,
+                                                                      'UNRECOGNIZED_VERSION',
+                                                                      ['MEETS_DEVICE_INTEGRITY'])
+    with raises(IllegalConfigurationException):
+        GooglePlayIntegrityApiConfig(decryption_key=base64.standard_b64encode(aes_key).decode(),
+                                     verification_key=base64.standard_b64encode(
+                                         verification_key.public_bytes(encoding=Encoding.DER,
+                                                                       format=PublicFormat.SubjectPublicKeyInfo))
+                                     .decode(),
+                                     apk_package_name=apk_package_name,
+                                     production=True,
+                                     allow_non_play_distribution=True,
+                                     verify_code_signature_hex=[]
+                                     )
